eb5cb6ecda03bc44f429d65ace77a048cc3a8d69
XOMAK12 <nnikitaivanov202@gmail.com>
2024-08-06 21:58:48 +0600

    lesson1

---
+++ lesson1/src/pages/mainPage/MainPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+const MainPage = () => {
+    const mainTitle = useSelector(state => state.title)
+
+    const dispatch = useDispatch()
+
+    const changeTitle = () => {
+        dispatch({
+            type: 'CHANGE_TITLE'
+        })
+    }
+
+    return (
+        <div>
+            <h1>{mainTitle}</h1>
+            <button onClick={changeTitle}>Change Title</button>
+        </div>
+    );
+};
+
+export default MainPage;
